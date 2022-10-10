df8c87ed727441250630a81624adfdb6d78ed632
purvapatil5 <purva5@uw.edu>
2022-10-09 19:05:09 -0700

    uploading complete project

---
+++ HW1/app/src/main/java/com/example/hw01/SecondActivity.java
@@ -0,0 +1,28 @@
+package com.example.hw01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        configureBackButton();
+
+    }
+
+    private void configureBackButton() {
+        Button backButton = (Button) findViewById(R.id.backbutton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
