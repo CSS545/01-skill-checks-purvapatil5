69e1be8764500354db68194dfa54ac35e51d4cee
purvapatil5 <purva5@uw.edu>
2022-10-23 23:06:58 -0700

    Commit for Basic Storage document and application

---
+++ MyApplication/app/src/main/java/com/example/myapplication/SecondActivity.java
@@ -0,0 +1,30 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        configureBackButton();
+
+    }
+
+    private void configureBackButton() {
+        Button backButton = (Button) findViewById(R.id.backbutton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
+
